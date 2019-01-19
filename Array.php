4b8ed3b514ab2fb30bc283271621b47d8ca6f737
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-01-19 18:05:08 +0530

    Adding Copyright in Array Page

--- Array.php
+++ Array.php
@@ -10,11 +10,18 @@
 			color: #ff0000;
 			text-shadow: 2px 2px 9px red;
 		}
+		small{
+			color: #444444;
+			background-color: #ddd; 
+			text-align: center;
+			padding: 2px;
+		}
 	</style>
 </head>
 <body>
 	<?php $games  = array("Hockey", "Cricket", "Badminton", "Football", "Shooting" );
-
+		$today = date("F j, Y");
+		$my_name = "Rishabh Mathur";
 	?>
 	<h1 style="text-align: center; color: #ddd;">Array</h1>
 		<h2><p style="margin-left: 45px;">Some of games are : </p></h2>
@@ -25,5 +32,7 @@
 		<li><?php echo $games[3]; ?></li>
 		<li><?php echo $games[4]; ?></li>
 	</ul>
+
+	<small> &copy; <?php echo $today; ?>  | <?php echo $my_name; ?></small>
 </body>
 </html>
