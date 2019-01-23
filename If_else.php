905d5de0bdfceb92bf272a85279cbc77761827cd
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-01-23 17:38:15 +0530

    If else statement in PHP git status

---
+++ If_else.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>If Else</title>
+	<style>
+		p{
+			
+			margin: 0px auto;
+			text-align: center;
+			color: blue;
+			font-style: italic;
+			background-color: #d3d3d3; 
+		}
+	</style>
+</head>
+<body>
+	<p><?php
+		$a=30;
+		$b=60;
+		if ($a < $b) {
+			echo "Yes $a is always smaller then $b";
+		}
+		?></p>
+
+</body>
+</html>
