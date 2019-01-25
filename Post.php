2ffb3bae07a6a6ea1af7b5991c05220160e87c45
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-01-25 16:34:13 +0530

    Posting at beginners level

---
+++ Post.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>POST in PHP</title>
+</head>
+<body>
+	<form action="welcome.php" method="POST">
+		Name <input type="text" name="name"><br>
+		Email <input type="email" name="email"><br><br>
+		<input type="submit" name="submit" value="Submit">
+	</form>
+</body>
+</html>
