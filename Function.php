f7186916775bac7276fedb3562d4536fecce36ea
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-01-25 17:49:13 +0530

    Added Custom function

--- Function.php
+++ Function.php
@@ -2,8 +2,18 @@
 <html>
 <head>
 	<title>Functions</title>
+	<style>
+		#first{
+			background-color: lightblue;
+		}
+		#second{
+			background-color: lightgray;
+			color: blue;
+		}
+	</style>
 </head>
-<body>
+<body><div id="first">
+	<p style="text-align: center; font-size: 40px; color: red;">Built-In Functions</p>
 	<h1>Using sort()</h1>
 		<?php 
 			$dinner = array("Dal", "Chawal", "Roti", "Papad");
@@ -20,5 +30,23 @@
 				echo "$games <br>";
 			}
 		?>
+		<h1>Using sha1()</h1>	
+		<?php 
+			$password = "My Password";
+			echo "Before : $password <br>";
+
+			$password = sha1($password);
+			echo "After : $password";
+		?>
+</div>
+<div id="second">
+	<p style="text-align: center; font-size: 40px; color: red;">Custom Functions</p>
+	<?php
+		function myIndia(){
+			echo "India is my Country, and it is very strong and powerfull";
+		}
+		myIndia();
+	?>
+</div>
 </body>
 </html>
