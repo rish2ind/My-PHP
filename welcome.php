9509e7ffdff19f5c1cf6fa85b9642bf6bcc72403
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-01-25 16:35:39 +0530

    Next page of post

---
+++ welcome.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>POST in PHP</title>
+</head>
+<body>
+		Welcome <?php echo $_POST["name"];?><br>
+		Your email is : <?php echo $_POST["email"];?>
+
+	</form>
+</body>
+</html>
