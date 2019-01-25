c1df4a0df84feec676528d1bafcbac8bf281c2ce
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-01-25 17:23:06 +0530

    Added CSS in the Form

--- Post.php
+++ Post.php
@@ -2,12 +2,35 @@
 <html>
 <head>
 	<title>POST in PHP</title>
+	<style>
+		#form{
+			margin: 0px auto;
+			background-color: lightgray;
+			border: 1px solid black;
+			width: 300px;
+			padding: 14px;
+			font-style: "Arial";
+			font-size: 18px;
+		}
+		.input{
+			font-size: 15px;
+			padding: 12px;
+			font-family: sans-serif;
+			margin: 0px 8px 3px 14px;
+			background-color: lightblue;
+			border: none;
+			border-bottom: 1px solid gray;
+		}
+		.submit{
+			cursor: pointer;
+		}
+	</style>
 </head>
 <body>
-	<form action="welcome.php" method="POST">
-		Name <input type="text" name="name"><br>
-		Email <input type="email" name="email"><br><br>
-		<input type="submit" name="submit" value="Submit">
+	<form id="form" action="welcome.php" method="POST">
+		Name <input class="input" type="text" name="name"><br><br>
+		Email <input class="input" type="email" name="email"><br><br>
+		<input class="submit" type="submit" name="submit" value="Submit">
 	</form>
 </body>
 </html>
