bace145072c51436d1b79e3c59499567841e9970
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-01-25 17:24:27 +0530

    Uses For Each loop

---
+++ ForEach.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ForEachLoop</title>
+</head>
+<body>
+		<?php
+			$places = array("Kashmir", "Rajasthan", "Mumbai" ); 
+			foreach ($places as $place) {
+				echo "My Favourite place is $place . <br>";
+			}
+			
+		?>	
+</body>
+</html>
