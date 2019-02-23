cc43c4a694b8a4a532e3aeef4ddfc6500aceae9a
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-02-23 17:08:15 +0530

    Delete data from Database...HTML code

---
+++ Delete.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Delete Data</title>
+</head>
+<body>
+	<form action="delete1.php" method="POST">
+		<label><input type="text" name="num" placeholder="mobile"></label>
+		<label><input type="submit" name="submit" value="submit"></label>
+	</form>
+</body>
+</html>
